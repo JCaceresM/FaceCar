5439739c14098dae710d539b93ed866986cf1763
Jonas Caceres Marte <jonad.caceres@gmail.com>
2021-03-01 14:28:45 -0400

    pobrando husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-
-import React from 'react';
-import {firebaseConfig} from './firebaseConfig';
-import 'firebase/firestore';
-import './styles/index.less';
-import { FirebaseAppProvider,   } from 'reactfire';
-import { Result } from 'antd';
-
-
+import React from 'react'
+import { firebaseConfig } from './firebaseConfig'
+import 'firebase/firestore'
+import './styles/index.less'
+import { FirebaseAppProvider } from 'reactfire'
+import { Result } from 'antd'
 
 function App(): React.ReactElement {
   return (
-    
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <Result />
-      
     </FirebaseAppProvider>
-  );
+  )
 }
 
-export default App;
+export default App
