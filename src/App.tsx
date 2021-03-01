041ef3877ce7099362eefc39c462ba3d6a38ced9
Jonas Caceres Marte <jonad.caceres@gmail.com>
2021-03-01 14:00:11 -0400

    Firebase agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 
 import React from 'react';
-import logo from './logo.svg';
+import {firebaseConfig} from './firebaseConfig';
+import 'firebase/firestore';
 import './styles/index.less';
+import { FirebaseAppProvider,   } from 'reactfire';
+import { Result } from 'antd';
+
+
 
 function App(): React.ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Result />
+      
+    </FirebaseAppProvider>
   );
 }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCep8UD6ftOXuwNp-OJyQidght6__lJg9o",
+  authDomain: "facecar-11bcf.firebaseapp.com",
+  projectId: "facecar-11bcf",
+  storageBucket: "facecar-11bcf.appspot.com",
+  messagingSenderId: "427227584362",
+  appId: "1:427227584362:web:085830e0f442069a367713",
+  measurementId: "G-810Z12ZDF5"
+};
